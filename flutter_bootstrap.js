99f3b9fdd67b3ba774e61f80bf6d3c72b905c7e5
Bruk Bedlu <2468.bruk@gmail.com>
2025-09-02 20:43:52 +0100

    Deploy latest build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1353938458"
+    serviceWorkerVersion: "3724493958"
   }
 });
