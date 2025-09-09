36d6b1112f6f7d96ba843ff328f8d48ae6322682
Bruk Bedlu <2468.bruk@gmail.com>
2025-09-09 12:39:26 +0100

    Update base href after repo rename

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "758903154"
+    serviceWorkerVersion: "3143043001"
   }
 });
